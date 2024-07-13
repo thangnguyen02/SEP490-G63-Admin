6f657b557732868d4b8e00eecfc922a60e6c175b
leopan <tu416164@gmail.com>
2024-07-13 16:17:44 +0700

    update user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,13 +4,21 @@ interface LoginData {
   email: string
   password: string
 }
-interface RegisterData {
+interface RegisterData1 {
   company: string
   taxCode: string
   firstName: string
   lastName: string
   email: string
   phone: string
+}
+interface RegisterData2 {
+  company: string
+  taxCode: string
+  presenter: string
+  email: string
+  phone: string
+  planpriceId: string
 }
 export const login = async ({ email, password }: LoginData) => {
   try {
@@ -20,7 +28,22 @@ export const login = async ({ email, password }: LoginData) => {
     console.log(error)
   }
 }
-export const register = async ({ company, taxCode, firstName, lastName, email, phone }: RegisterData) => {
+export const registerUser = async ({ company, taxCode, presenter, email, phone, planpriceId }: RegisterData2) => {
+  try {
+    const response = await adminInstance.post('public/auth/register', {
+      companyName: company,
+      taxCode,
+      presenter,
+      email,
+      phone,
+      planpriceId
+    })
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+export const register = async ({ company, taxCode, firstName, lastName, email, phone }: RegisterData1) => {
   try {
     const response = await adminInstance.post('', { company, taxCode, firstName, lastName, email, phone })
     return response.data
