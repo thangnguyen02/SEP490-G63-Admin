ed2eea3c5f1dce53fefe1af9925a76de6bf34145
leopan <tu416164@gmail.com>
2024-07-18 15:10:22 +0700

    fix footer

--- src/components/landing_page/Footer/Footer.tsx
+++ src/components/landing_page/Footer/Footer.tsx
@@ -27,24 +27,20 @@ const Footer = () => {
   return (
     <>
       <div className=' dark:bg-gray-950 py-10 relative overflow-hidden'>
-        {/* <video autoPlay loop muted className='absolute right-0 top-0 h-full overflow-hidden w-full object-cover z-[-1]'>
-          <source src={NatureVid} type='video/mp4' />
-        </video> */}
         <img
           src={ImgFooter}
           className='absolute right-0 top-0 h-full overflow-hidden w-full object-cover z-[-1]'
           alt=''
         />
-        <div className='container'>
-          <div className='grid md:grid-cols-3 py-5 bg-gray-200 rounded-t-xl'>
+        <div className='container mx-auto w-[98%]'>
+          <div className='grid md:grid-cols-3 bg-gray-200 rounded-t-xl items-center text-justify'>
             <div className='py-8 px-4'>
               <h1 className='flex items-center gap-3 text-xl sm:text-3xl font-bold text-justify sm:text-left'>
                 <img src={FooterLogo} alt='' className='max-h-[60px] color-white' />
                 {/* TravelloGo */}
               </h1>
-              <p className='text-sm'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde facere ab hic accusamus omnis dolor
-                voluptatibus illo, tempore eum tenetur.
+              <p className='text-sm mt-2'>
+                With TDocMan system, users can manage, store and sign contracts online easily and conveniently.
               </p>
               <br />
               <div className='flex items-center gap-3 '>
@@ -73,7 +69,7 @@ const Footer = () => {
             <div className='grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10'>
               <div>
                 <div className='py-8 px-4'>
-                  <h1 className='text-xl font-bold text-justify sm:text-left mb-3'>Important Links</h1>
+                  <h1 className='text-xl font-bold text-justify sm:text-left mb-3'>Liên hệ</h1>
                   <ul className='flex flex-col gap-3'>
                     {FooterLinks.map((link) => (
                       <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 dark:text-black-500'>
@@ -88,7 +84,7 @@ const Footer = () => {
               </div>
               <div>
                 <div className='py-8 px-4'>
-                  <h1 className='text-xl font-bold text-justify sm:text-left mb-3'>Important Links</h1>
+                  <h1 className='text-xl font-bold text-justify sm:text-left mb-3'>Dịch vụ</h1>
                   <ul className='flex flex-col gap-3'>
                     {FooterLinks.map((link) => (
                       <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 dark:text-black-500'>
@@ -103,7 +99,7 @@ const Footer = () => {
               </div>
               <div>
                 <div className='py-8 px-4'>
-                  <h1 className='text-xl font-bold text-justify sm:text-left mb-3'>Important Links</h1>
+                  <h1 className='text-xl font-bold text-justify sm:text-left mb-3'>Chính sách</h1>
                   <ul className='flex flex-col gap-3'>
                     {FooterLinks.map((link) => (
                       <li className='cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 dark:text-black-500'>
@@ -117,10 +113,10 @@ const Footer = () => {
                 </div>
               </div>
             </div>
-            <p className='text-center items-center justify-center mx-auto border-t-2 border-gray-300/50 bg-primary text-black'>
-              @copyright 2024 All rights reserved || Made with ❤️ by G-63
-            </p>
           </div>
+          <p className='flex-1 flex-shrink bg-primary text-black bg-gray-200 text-center py-2 border-t-2 border-gray-300'>
+            @copyright 2024 All rights reserved || Made with ❤️ by G-63
+          </p>
         </div>
       </div>
     </>
