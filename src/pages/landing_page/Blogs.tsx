6a1a10018e968cce6d303206c5b33d0cbe3fd9b6
leopan <tu416164@gmail.com>
2024-08-03 17:50:16 +0700

    optimal

--- src/pages/landing_page/Blogs.tsx
+++ src/pages/landing_page/Blogs.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import BlogsComp from "~/components/landing_page/Blogs/BlogsComp";
+import React from 'react'
+import BlogsComp from '~/components/landing_page/Blogs/BlogsComp'
+import Navbar from '~/components/landing_page/Navbar/Navbar'
 
 const Blogs = () => {
   return (
-    <div className="min-h-screen pt-14 bg-gray-100">
-      <BlogsComp />
-    </div>
-  );
-};
+    <>
+      <Navbar />
+      <div className='min-h-screen pt-14 bg-gray-100'>BLOG</div>
+    </>
+  )
+}
 
-export default Blogs;
+export default Blogs
