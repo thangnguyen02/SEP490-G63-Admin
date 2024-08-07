3531901a038d9fe320f3e9e05ec221de35805eb0
leopan <tu416164@gmail.com>
2024-08-07 17:22:48 +0700

    notification

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -1,9 +1,13 @@
-import { adminInstance } from '../config/axiosConfig.ts'
+import { adminInstance } from '~/config/axiosConfig'
 export const getNumberUnreadNotify = async () => {
   const response = await adminInstance.get(`notification/unread`)
   return response.data
 }
 export const readNotify = async (id: string) => {
   const response = await adminInstance.put(`notification/${id}/true`)
+  return response.data
+}
+export const deleteNotify = async (id: string) => {
+  const response = await adminInstance.delete(`notification/${id}`)
   return response.data
 }
